9b34ca058c38f4f3973decf6165b4a3f1a9a0e9f
EmmanuelMOUMBOUILOU <emmanuelmoumbouilou@gmail.com>
2026-09-04 17:01:26 +0200

    Corriger la syntaxe de la commande chown dans le script 8-log_policy.sh pour assurer une meilleure compatibilité

--- linux_security/1x00_linux_fundamentals/8-log_policy.sh
+++ linux_security/1x00_linux_fundamentals/8-log_policy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-mkdir -p "$1" && chown root:$2 "$1" && chmod 2770 "$1"
+mkdir -p "$1" && chown root:$2 "$1" && chmod 2750 $1
 printf '%s\n' "$1/*.log {" " create 0640 root $2" " rotate 7" " daily" "}" > /etc/logrotate.d/app
