1ee23425dbe4cf5be0f56299a60f37a15cad9392
EmmanuelMOUMBOUILOU <emmanuelmoumbouilou@gmail.com>
2026-09-04 16:53:31 +0200

    Corriger la propriété du propriétaire dans le script 8-log_policy.sh pour assurer que le propriétaire est toujours défini sur root

--- linux_security/1x00_linux_fundamentals/8-log_policy.sh
+++ linux_security/1x00_linux_fundamentals/8-log_policy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-mkdir -p "$1" && chown :"$2" "$1" && chmod 2770 "$1"
-printf '%s\n' "$1/*.log {" "    create 0640 root $2" "    rotate 7" "    daily" "}" > /etc/logrotate.d/app
+mkdir -p "$1" && chown root:"$2" "$1" && chmod 2770 "$1"
+printf '%s\n' "$1/*.log {" " create 0640 root $2" " rotate 7" " daily" "}" > /etc/logrotate.d/app
