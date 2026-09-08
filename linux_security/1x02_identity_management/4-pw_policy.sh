a1761638fe35db4a2362d3161a32cb299e838f0c
EmmanuelMOUMBOUILOU <emmanuelmoumbouilou@gmail.com>
2026-09-08 16:20:20 +0200

    Refactor le script 4-pw_policy.sh pour améliorer la lisibilité et la structure des conditions

--- linux_security/1x02_identity_management/4-pw_policy.sh
+++ linux_security/1x02_identity_management/4-pw_policy.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-dpkg -s "$1" >/dev/null 2>&1 || apt-get update && apt-get install -y "$1"
-grep -q '^password.*pam_pwquality.so' "$2" && sed -i -E 's|^password.*pam_pwquality.so.*|password requisite pam_pwquality.so minlen=12 minclass=3|' "$2" || echo 'password requisite pam_pwquality.so minlen=12 minclass=3' >> "$2"
+if ! dpkg -s "$1" >/dev/null 2>&1; then
+    apt-get update
+    apt-get install -y "$1"
+fi
+
+if grep -q '^password.*pam_pwquality.so' "$2"; then
+    sed -i -E 's|^password.*pam_pwquality.so.*|password requisite pam_pwquality.so minlen=12 minclass=3|' "$2"
+else
+    echo 'password requisite pam_pwquality.so minlen=12 minclass=3' >> "$2"
+fi
