d9207f4b2c17d2c5b32089e2247010f7d756ea21
EmmanuelMOUMBOUILOU <emmanuelmoumbouilou@gmail.com>
2026-09-08 16:23:43 +0200

    Corriger l'installation du paquet dans le script 4-pw_policy.sh en supprimant les guillemets autour de $1

--- linux_security/1x02_identity_management/4-pw_policy.sh
+++ linux_security/1x02_identity_management/4-pw_policy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if ! dpkg -s "$1" >/dev/null 2>&1; then
     apt-get update
-    apt-get install -y "$1"
+    apt-get install -y $1
 fi
 
 if grep -q '^password.*pam_pwquality.so' "$2"; then
