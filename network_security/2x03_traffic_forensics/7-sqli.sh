fbff8f857a55a735fa1a062a1402a24c50466459
EmmanuelMOUMBOUILOU <emmanuelmoumbouilou@gmail.com>
2026-09-21 15:27:15 +0200

    Mise à jour du script 7-sqli.sh pour utiliser des filtres tshark plus précis pour détecter les requêtes SQL injectées

--- network_security/2x03_traffic_forensics/7-sqli.sh
+++ network_security/2x03_traffic_forensics/7-sqli.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-tshark -r "$1" -T fields -e http.request.full_uri | grep -Ei 'UNION|SELECT|%55%4E%49%4F%4E|%53%45%4C%45%43%54'
+tshark -r "$1" -Y 'http.request.uri contains "UNION" || http.request.uri contains "SELECT" || http.request.uri contains "union" || http.request.uri contains "select"' -T fields -e http.request.uri
