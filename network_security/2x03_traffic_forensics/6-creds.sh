8b0266e94e103b14c75d5baffbf16b42b01d9d8a
EmmanuelMOUMBOUILOU <emmanuelmoumbouilou@gmail.com>
2026-09-21 15:21:25 +0200

    Inversion des champs dans le script 6-creds.sh pour extraire correctement les valeurs des formulaires URL encodés contenant des mots de passe

--- network_security/2x03_traffic_forensics/6-creds.sh
+++ network_security/2x03_traffic_forensics/6-creds.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-tshark -r "$1" -T fields -e urlencoded-form.key -e urlencoded-form.value | awk -F'\t' '$1 ~ /^(password|pass|pwd)$/ {print $2}'
+tshark -r "$1" -T fields -e urlencoded-form.value -e urlencoded-form.key | awk -F'\t' '$2 ~ /^(password|pass|pwd)$/ {print $1}'
